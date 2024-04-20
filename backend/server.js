3faffc89aeeefb57400e8313cdcae439dd6e7bc0
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-04-20 06:34:57 +0545

    Creating Backend of Food App

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,17 @@ import express from "express"
 import cors from "cors"
 
 //app config
-const app = express(); //initialie app with express package
-const port = 4000;                   //define port number where our server will be running
+const app = express()
+const port = 4000
 
-
-//initializing middleware
-app.use(express.json()); //using middleware whenever we will get request from frontend to backend , that will be parsed using json
-app.use(cors()) // using this we can access  backend from any  forntend
-
+//middleware
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("API Working")
-})    // get method is http method, using this we can request data from server
-
+})
 
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
-}) //running server
-
+})
